5684e76dd155375ca35446c639c32ef21bb2d8c9
maronana <reina.ciel.0510@gmail.com>
2022-11-14 12:32:15 +0900

    docsフォルダへ変更

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 8080,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     port: 8080,
+//   }
+// })
+module.exports = {
+    outputDir: 'docs',
+    assetsDir: './',
+    publicPath: './'
   }
-})
